09f5e88a4e916748d7e7f1a42bbbffabb9e4e596
Everson33rj <eversonsilvadeveloper@gmail.com>
2024-10-21 01:59:08 -0300

    Alteração da rota das páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AppStyled } from "./AppStyled";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -8,14 +8,12 @@ import Contato from "./components/pages/Contato";
 function App() {
   return (
     <AppStyled>
-      <Router basename="/curriculum">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/contato" element={<Contato />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Header />
+      <Routes>
+        <Route path="/curriculum" element={<Main />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer />
     </AppStyled>
   );
 }
